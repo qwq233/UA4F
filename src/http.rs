8c5a5c15cf83461e81f43ccf4ea5c06457ac2316
qwq233 <qwq233@qwq2333.top>
2024-11-09 23:23:41 +0800

    feat: optimise whitelist UA detect
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- src/http.rs
+++ src/http.rs
@@ -1,35 +1,17 @@
 use log::{debug, error};
 
 pub fn is_http_request(buf: &mut [u8]) -> bool {
-    if buf[0..3] == [b'G', b'E', b'T'] {
-        // GET
-        true
-    } else if buf[0..4] == [b'P', b'O', b'S', b'T'] {
-        // POST
-        true
-    } else if buf[0..4] == [b'H', b'E', b'A', b'D'] {
-        // HEAD
-        true
-    } else if buf[0..6] == [b'D', b'E', b'L', b'E', b'T', b'E'] {
-        // DELETE
-        true
-    } else if buf[0..5] == [b'T', b'R', b'A', b'C', b'E'] {
-        // TRACE
-        true
-    } else if buf[0..7] == [b'O', b'P', b'T', b'I', b'O', b'N', b'S'] {
-        // OPTIONS
-        true
-    } else if buf[0..7] == [b'C', b'O', b'N', b'N', b'E', b'C', b'T'] {
-        // CONNECT
-        true
-    } else if buf[0..3] == [b'P', b'U', b'T'] {
-        // PUT
-        true
-    } else if buf[0..5] == [b'P', b'A', b'T', b'C', b'H'] {
-        // PATCH
-        true
-    } else {
-        false
+    match buf {
+        [b'G', b'E', b'T', ..] => true,
+        [b'P', b'O', b'S', b'T', ..] => true,
+        [b'H', b'E', b'A', b'D', ..] => true,
+        [b'D', b'E', b'L', b'E', b'T', b'E', ..] => true,
+        [b'T', b'R', b'A', b'C', b'E', ..] => true,
+        [b'O', b'P', b'T', b'I', b'O', b'N', b'S', ..] => true,
+        [b'C', b'O', b'N', b'N', b'E', b'C', b'T', ..] => true,
+        [b'P', b'U', b'T', ..] => true,
+        [b'P', b'A', b'T', b'C', b'H', ..] => true,
+        _ => false,
     }
 }
 
@@ -75,17 +57,11 @@ pub fn modify_user_agent(buf: &mut Vec<u8>, user_agent: &String) {
     debug!("start: {}, end: {}", start, end);
     debug!("user_agent: {}", String::from_utf8_lossy(&buf[start..end]));
 
-    if check_is_in_whitelist(buf[start + 12..end].to_vec()) {
+    if check_is_in_whitelist(&buf[start + 12..end]) {
         return;
     }
 
-    buf.drain(start + 12..end - 1);
-    let user_agent = user_agent.as_bytes();
-    let mut len = user_agent.len();
-    while len > 0 {
-        buf.insert(start + 12, user_agent[len - 1]);
-        len -= 1;
-    }
+    buf.splice(start + 12..end - 1, user_agent.bytes());
 
     debug!(
         "new user_agent: {}",
@@ -93,22 +69,8 @@ pub fn modify_user_agent(buf: &mut Vec<u8>, user_agent: &String) {
     );
 }
 
-fn check_is_in_whitelist(buf: Vec<u8>) -> bool {
-    let buf = String::from_utf8_lossy(&buf);
-    let buf = buf.to_lowercase();
-    let buf = buf.trim();
-
-    let whitelist = ["MicroMessenger Client", "bilibili"];
-    let whitelist = whitelist
-        .iter()
-        .map(|x| x.to_lowercase())
-        .collect::<Vec<String>>();
-    for i in whitelist.iter() {
-        if buf.contains(i) {
-            debug!("{} found, skip.", i);
-            return true;
-        }
-    }
-
-    false
+fn check_is_in_whitelist(buf: &[u8]) -> bool {
+    const WHITELIST: &[&str] = &["MicroMessenger Client", "bilibili"];
+    let buf = std::str::from_utf8(buf).unwrap_or("").to_lowercase();
+    WHITELIST.iter().any(|&item| buf.contains(item))
 }
